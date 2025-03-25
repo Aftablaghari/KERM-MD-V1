0eb2ac950bec567d122b6c45f0bd462ad6985604
Aftablaghari <laghariaftab656@gmail.com>
2025-03-25 15:53:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+SESSION_ID: process.env.SESSION_ID || "KERM-MD-V1~jQQ2EArD#I2uV92giGh4vsePYNDk7u0jJaZO23fDQRQT31aWJwME",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "private",
+MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
